3f3c1b237f81060dda7674b35dd52cbb80e03823
Neema <patelneema93@gmail.com>
2024-07-29 13:49:48 -0400

    Action class create

--- indecision/src/App.js
+++ indecision/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Header from './Component/Header';
+import Action from './Component/Action';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Action />
     </div>
   );
 }
---
+++ indecision/src/Component/Action.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class Action extends Component{
+    render() {
+        return(
+            <div>
+                <button >What should I do?</button>
+            </div>
+        )
+    }
+}
+export default Action;
