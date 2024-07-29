0fe4aeafe7e178dff6772eef15b60b4a69298a69
Neema <patelneema93@gmail.com>
2024-07-29 15:13:23 -0400

    handle add option

--- indecision/src/Component/AddOption.jsx
+++ indecision/src/Component/AddOption.jsx
@@ -1,8 +1,24 @@
 import { Component } from "react";
 
 class AddOption extends Component{
+    handleAddOption(e){
+        e.preventDefult();
+        const option = e.target.elements.option.value.trim();
+        if(option){
+            alert('Hello',option);
+            console.log(option);
+        }
+    }
     render(){
-        return(<div> add option</div>);
+        return(
+        <div> 
+          
+          <form  >
+            <input type="text" name="option" />
+            <button onSubmit={this.handleAddOption}>Add option</button>
+          </form>
+        </div>
+        );
     }
 }
 export default AddOption;
