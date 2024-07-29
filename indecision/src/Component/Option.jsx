899984ac011ba6c98e4759821eeb952c5ee6ffd6
Neema <patelneema93@gmail.com>
2024-07-29 14:08:16 -0400

    option class create

---
+++ indecision/src/Component/Option.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class Option extends Component{
+    render(){
+        return(
+            <div>
+                OptionCompont Here
+            </div>
+        );
+    }
+}
+export default Option;
